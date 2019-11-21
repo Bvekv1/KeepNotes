f011c4f36ef3cae8bb321aefde80f2a5005fa8d9
Bvekv1 <bvekp1@gmail.com>
2019-11-21 06:11:44 +0545

    Layout Design for Login screen and Register

--- app/src/main/java/com/bibek/keepnotes/MainActivity.java
+++ app/src/main/java/com/bibek/keepnotes/MainActivity.java
@@ -3,12 +3,21 @@ package com.bibek.keepnotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvHeader, tvNoAccount;
+    private EditText  etUserName, etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvHeader = findViewById(R.id.tvHeader);
+        tvNoAccount = findViewById(R.id.tvNoAccount);
+        
     }
 }
